b29d0ea1ae89d98fef4490b5760b1c198fa09903
rkaynor <92887092+mkaynor@users.noreply.github.com>
2023-02-08 14:40:47 -0500

    Added nav-menu component

--- frontend/crave-finder/src/app/app.module.ts
+++ frontend/crave-finder/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { MenuComponent } from './menu/menu.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    MenuComponent
+    MenuComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    DragDropModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
