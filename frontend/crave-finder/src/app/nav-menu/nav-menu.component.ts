e2d999cf69ce48ce0d75cfb75a187dc6306d90e5
ZacharyBournand <zachary.bournand@ufl.edu>
2023-03-01 19:17:36 -0500

    Connected front-end to back-end for user logout

--- frontend/crave-finder/src/app/nav-menu/nav-menu.component.ts
+++ frontend/crave-finder/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
 
+}*/
+
+
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+
+export class NavMenuComponent /*implements AfterViewInit*/ {
+  responseMessage: string = '';
+
+  user = {
+    username: '',
+    password: ''
+  };
+
+  @ViewChild('refLogoutLink', { static: false, read: ElementRef }) logoutLink!: ElementRef;
+
+  constructor(private http: HttpClient/*, private renderer: Renderer2, private el: ElementRef*/) {}
+
+  ngAfterViewInit() {
+    const logoutLink = document.querySelector('a.logout') as HTMLElement;
+
+    if (logoutLink) {
+      console.log("logoutLink:", logoutLink);
+      logoutLink.addEventListener('click', (event: Event) => {
+        event.preventDefault();
+        this.logout();
+      });
+    }
+  }
+
+  logout(): void {
+    // Log the user out
+    //this.isAuthenticated = false;
+    this.http.post('http://localhost:8080/logout', {}).subscribe((response: any) => {
+      console.log(response);
+      this.responseMessage = response.message;
+    });
+  }
 }
