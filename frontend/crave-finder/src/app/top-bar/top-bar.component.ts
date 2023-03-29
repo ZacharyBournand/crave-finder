bdf05b2d04269804c86fec4d6486a687c579334a
rkaynor <92887092+mkaynor@users.noreply.github.com>
2023-03-29 19:49:37 -0400

    Fixed reviews page not working when logged in

--- frontend/crave-finder/src/app/top-bar/top-bar.component.ts
+++ frontend/crave-finder/src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
+import { UserService } from '../user.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,12 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 
-export class TopBarComponent 
+export class TopBarComponent implements OnInit
 {
   isLoggedIn: boolean;
+  user: any
 
-  constructor() 
+  constructor(private UserService:UserService) 
   {
     this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    this.UserService.getUser.subscribe(usr => this.user = usr);
   }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('isLoggedIn') == 'true')
+    {
+      this.isLoggedIn = true;
+    }
+  }
+
 }
